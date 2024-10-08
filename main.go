067e37448081cc3798bfc99ed9eb1476e0ba6178
Eduard S. <eduardsanou@posteo.net>
2024-10-08 23:18:08 +0200

    support structs 1 level

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"os"
 	"runtime/debug"
+	"strconv"
 	"strings"
 )
 
@@ -22,15 +23,42 @@ const (
 	VarNext
 )
 
+type VarsType int
+
+const (
+	VarsSingle VarOpt = iota
+	VarsTuple
+	VarsStruct
+)
+
+type Vars struct {
+	Type VarsType
+}
+
 type Var struct {
 	name   string
 	size   int
 	signed bool
+	value  *int64
+}
+
+func (v *Var) EmitDeclare(t *Translator) {
+	if v.value == nil {
+		t.Printf("%v %v;\n", typeFmt(v.signed, v.size), v.name)
+	} else {
+		t.Printf("parameter [%v:0] %v = %v;\n", v.size-1, v.name, *v.value)
+	}
+}
+
+type VarVer struct {
+	size    int
+	signed  bool
+	version int
 }
 
 type Block struct {
 	// Mapping of variable name -> SSA version
-	vars       map[string]int
+	vars       map[string]VarVer
 	returnVars []Var
 }
 
@@ -41,6 +69,7 @@ type Translator struct {
 	constCnt  int
 	typeInfo  types.Info
 	funcs     map[string]FuncArgs
+	structs   map[string]Struct
 	// Mapping of block depth -> Block
 	blocks []Block
 }
@@ -53,12 +82,13 @@ func NewTranslator(out io.Writer, typeInfo types.Info) Translator {
 		out:       out,
 		typeInfo:  typeInfo,
 		funcs:     make(map[string]FuncArgs),
+		structs:   make(map[string]Struct),
 		blocks:    []Block{},
 	}
 }
 
 func (t *Translator) PushBlock() {
-	t.blocks = append(t.blocks, Block{vars: make(map[string]int)})
+	t.blocks = append(t.blocks, Block{vars: make(map[string]VarVer)})
 }
 
 func (t *Translator) PopBlock() {
@@ -101,10 +131,8 @@ func (t *Translator) curBlock() *Block {
 	return &t.blocks[len(t.blocks)-1]
 }
 
-func (t *Translator) newVar(name string, typ types.Type) Var {
-	t.curBlock().vars[name] = 0
-
-	signed, size := t.decodeType(typ)
+func (t *Translator) NewVar(name string, signed bool, size int) Var {
+	t.curBlock().vars[name] = VarVer{signed: signed, size: size, version: 0}
 	return Var{
 		name:   fmt.Sprintf("%v0", name),
 		signed: signed,
@@ -112,10 +140,9 @@ func (t *Translator) newVar(name string, typ types.Type) Var {
 	}
 }
 
-func (t *Translator) NewVar(name string, nTyp ast.Expr) Var {
-	t.curBlock().vars[name] = 0
-
+func (t *Translator) NewVarExpr(name string, nTyp ast.Expr) Var {
 	signed, size := t.DecodeType(nTyp)
+	t.curBlock().vars[name] = VarVer{signed: signed, size: size, version: 0}
 	return Var{
 		name:   fmt.Sprintf("%v0", name),
 		signed: signed,
@@ -123,31 +150,30 @@ func (t *Translator) NewVar(name string, nTyp ast.Expr) Var {
 	}
 }
 
-func (t *Translator) GetVar(name string, nTyp ast.Expr) Var {
-	ver, ok := t.curBlock().vars[name]
+func (t *Translator) GetVar(name string) Var {
+	v, ok := t.curBlock().vars[name]
 	if !ok {
 		panic(fmt.Errorf("Var \"%v\" not found in current block", name))
 	}
-	signed, size := t.DecodeType(nTyp)
 	return Var{
-		name:   fmt.Sprintf("%v%v", name, ver),
-		signed: signed,
-		size:   size,
+		name:   fmt.Sprintf("%v%v", name, v.version),
+		signed: v.signed,
+		size:   v.size,
 	}
 }
 
-func (t *Translator) GetVarNextVer(name string, nTyp ast.Expr) Var {
+func (t *Translator) GetVarNextVer(name string) Var {
 	block := t.curBlock()
-	ver, ok := block.vars[name]
+	v, ok := block.vars[name]
 	if !ok {
 		panic(fmt.Errorf("Var \"%v\" not found in current block", name))
 	}
-	block.vars[name] = ver + 1
-	signed, size := t.DecodeType(nTyp)
+	v.version += 1
+	block.vars[name] = v
 	return Var{
-		name:   fmt.Sprintf("%v%v", name, ver+1),
-		signed: signed,
-		size:   size,
+		name:   fmt.Sprintf("%v%v", name, v.version),
+		signed: v.signed,
+		size:   v.size,
 	}
 }
 
@@ -173,12 +199,25 @@ func (t *Translator) newAnonVar(typ types.Type) Var {
 	return v
 }
 
-func (t *Translator) NewAnonConst(nTyp ast.Expr) Var {
+func (t *Translator) NewAnonConst(nTyp ast.Expr, value int64) Var {
 	signed, size := t.DecodeType(nTyp)
 	v := Var{
 		name:   fmt.Sprintf("c%v", t.constCnt),
 		signed: signed,
 		size:   size,
+		value:  &value,
+	}
+	t.constCnt += 1
+	return v
+}
+
+func (t *Translator) newAnonConst(typ types.Type, value int64) Var {
+	signed, size := t.decodeType(typ)
+	v := Var{
+		name:   fmt.Sprintf("c%v", t.constCnt),
+		signed: signed,
+		size:   size,
+		value:  &value,
 	}
 	t.constCnt += 1
 	return v
@@ -196,32 +235,70 @@ func (t *Translator) Printf(format string, a ...any) {
 	t.errcheck(err)
 }
 
+func (t *Translator) VarVars(name string, s ast.Expr, varOpt VarOpt) []Var {
+	_, isStruct := t.typeInfo.TypeOf(s).Underlying().(*types.Struct)
+	var vars []Var
+
+	if isStruct {
+		typeName := t.typeInfo.TypeOf(s).(*types.Named).Obj().Name()
+		fields := t.structs[typeName].fields
+		switch varOpt {
+		case VarNew:
+			for _, field := range fields {
+				vr := t.NewVar(fmt.Sprintf("%v_%v", name, field.name), field.signed, field.size)
+				vars = append(vars, vr)
+			}
+		case VarGet:
+			for _, field := range fields {
+				vr := t.GetVar(fmt.Sprintf("%v_%v", name, field.name))
+				vars = append(vars, vr)
+			}
+		// case VarNext:
+		// 	vr = t.GetVarNextVer(name, s)
+		// 	vr.EmitDeclare(t)
+		default:
+			panic("TODO")
+		}
+	} else {
+		switch varOpt {
+		case VarNew:
+			vars = append(vars, t.NewVarExpr(name, s))
+		case VarGet:
+			vars = append(vars, t.GetVar(name))
+		case VarNext:
+			vars = append(vars, t.GetVarNextVer(name))
+		}
+	}
+	return vars
+}
+
+func (t *Translator) IdentVars(s *ast.Ident, varOpt VarOpt) []Var {
+	return t.VarVars(s.Name, s, varOpt)
+}
+
 func (t *Translator) EmitExpr(x ast.Expr, varOpt VarOpt) []Var {
 	switch s := x.(type) {
 	case *ast.BinaryExpr:
 		vx := t.EmitExpr(s.X, VarGet)[0]
 		vy := t.EmitExpr(s.Y, VarGet)[0]
 		vr := t.NewAnonVar(s)
-		t.Printf("%v %v;\n", typeFmt(vr.signed, vr.size), vr.name)
+		vr.EmitDeclare(t)
 		op := t.TranslateOp(s.Op)
 		t.Printf("assign %v = %v %v %v;\n", vr.name, vx.name, op, vy.name)
 		return []Var{vr}
 	case *ast.Ident:
-		var vr Var
-		switch varOpt {
-		case VarNew:
-			vr = t.NewVar(s.Name, s)
-			t.Printf("%v %v;\n", typeFmt(vr.signed, vr.size), vr.name)
-		case VarGet:
-			vr = t.GetVar(s.Name, s)
-		case VarNext:
-			vr = t.GetVarNextVer(s.Name, s)
-			t.Printf("%v %v;\n", typeFmt(vr.signed, vr.size), vr.name)
+		vars := t.IdentVars(s, varOpt)
+		if varOpt == VarNew || varOpt == VarNext {
+			for _, vr := range vars {
+				vr.EmitDeclare(t)
+			}
 		}
-		return []Var{vr}
+		return vars
 	case *ast.BasicLit:
-		vr := t.NewAnonConst(s)
-		t.Printf("parameter [%v:0] %v = %v;\n", vr.size-1, vr.name, s.Value)
+		value, err := strconv.ParseInt(s.Value, 10, 64)
+		t.errcheck(err)
+		vr := t.NewAnonConst(s, value)
+		vr.EmitDeclare(t)
 		return []Var{vr}
 	case *ast.CallExpr:
 		ident := s.Fun.(*ast.Ident)
@@ -229,18 +306,18 @@ func (t *Translator) EmitExpr(x ast.Expr, varOpt VarOpt) []Var {
 
 		var inputs []Var
 		for _, arg := range s.Args {
-			inputs = append(inputs, t.EmitExpr(arg, VarGet)[0])
+			inputs = append(inputs, t.EmitExpr(arg, VarGet)...)
 		}
 		var outputs []Var
 		if len(fn.outputs) == 1 {
 			vr := t.NewAnonVar(s)
-			t.Printf("%v %v;\n", typeFmt(vr.signed, vr.size), vr.name)
+			vr.EmitDeclare(t)
 			outputs = append(outputs, vr)
 		} else if len(fn.outputs) > 1 {
 			tuple := t.typeInfo.TypeOf(s).(*types.Tuple)
 			for i := 0; i < tuple.Len(); i++ {
 				vr := t.newAnonVar(tuple.At(i).Type())
-				t.Printf("%v %v;\n", typeFmt(vr.signed, vr.size), vr.name)
+				vr.EmitDeclare(t)
 				outputs = append(outputs, vr)
 			}
 		}
@@ -255,6 +332,71 @@ func (t *Translator) EmitExpr(x ast.Expr, varOpt VarOpt) []Var {
 		}
 		t.Printf(");\n")
 		return outputs
+	case *ast.CompositeLit:
+		// fmt.Printf("DBG %+#v\n", s.Type.(*ast.Ident).Name)
+		// fmt.Printf("DBG typeof %+#v\n", t.typeInfo.TypeOf(s).Underlying())
+		underlying := t.typeInfo.TypeOf(s).Underlying()
+		switch underlying.(type) {
+		case *types.Struct:
+			// Return all the struct fields in order (with 0 for defaults)
+			kvs := make(map[string]Var)
+			for _, elt := range s.Elts {
+				kv := elt.(*ast.KeyValueExpr)
+				value := t.EmitExpr(kv.Value, VarGet)[0]
+				kvs[kv.Key.(*ast.Ident).Name] = value
+			}
+			st := t.structs[s.Type.(*ast.Ident).Name]
+			var vars []Var
+			for _, field := range st.fields {
+				vr, ok := kvs[field.name]
+				if ok {
+					vars = append(vars, vr)
+				} else {
+					vr := t.newAnonConst(types.Typ[types.Bool], 0)
+					vr.EmitDeclare(t)
+					vr.signed = field.signed
+					vr.size = field.size
+					vars = append(vars)
+				}
+			}
+			return vars
+		default:
+			panic(fmt.Errorf("unsupported CompositeLit type: %+T", underlying))
+		}
+
+		// switch t.typeInfo.TypeOf(s.Type) {
+		// }
+		// for _, elt := range s.Elts {
+		// 	vars = append(vars, t.EmitExpr(elt, GetVar))
+		// }
+		// if st, ok := t.structs[s.Type.(*ast.Ident).Name]; ok {
+		// 	fmt.Printf("DBG %+#v\n", s)
+		// 	var vars []Var
+		// 	for _, elt := range s.Elts {
+		// 		vars = append(vars, t.EmitExpr(elt, GetVar))
+		// 	}
+		// } else {
+		// 	panic("unimplemented")
+		// }
+	case *ast.KeyValueExpr:
+		vl := t.EmitExpr(s.Key, VarNew)[0]
+		vr := t.EmitExpr(s.Value, VarGet)[0]
+		t.Printf("assign %v = %v;\n", vl.name, vr.name)
+		return []Var{vl}
+	case *ast.SelectorExpr:
+		vars := t.EmitExpr(s.X, VarGet)
+		_, isStruct := t.typeInfo.TypeOf(s.X).Underlying().(*types.Struct)
+		if !isStruct {
+			panic("unsupported")
+		}
+		typeName := t.typeInfo.TypeOf(s.X).(*types.Named).Obj().Name()
+		st := t.structs[typeName]
+		for i, field := range st.fields {
+			if field.name == s.Sel.Name {
+				return []Var{vars[i]}
+			}
+		}
+		panic("unreachable")
 	default:
 		panic(fmt.Errorf("unsupported Expr: %+T", x))
 	}
@@ -278,10 +420,14 @@ func (t *Translator) EmitAssignStmt(x *ast.AssignStmt) {
 		i := 0
 		for _, rhs := range x.Rhs {
 			results := t.EmitExpr(rhs, VarGet)
-			for _, r := range results {
-				vl := t.EmitExpr(x.Lhs[i], VarNew)[0]
-				t.Printf("assign %v = %v;\n", vl.name, r.name)
+			j := 0
+			for j < len(results) {
+				vls := t.EmitExpr(x.Lhs[i], VarNew)
 				i += 1
+				for _, vl := range vls {
+					t.Printf("assign %v = %v;\n", vl.name, results[j].name)
+					j += 1
+				}
 			}
 		}
 	case token.ASSIGN:
@@ -388,15 +534,18 @@ func (t *Translator) EmitFuncDecl(x *ast.FuncDecl) {
 	t.Printf("module %v (\n", x.Name)
 	for _, field := range x.Type.Params.List {
 		for _, name := range field.Names {
-			v := t.NewVar(name.Name, field.Type)
-			t.Printf("  input %v %v,\n", typeFmt(v.signed, v.size), v.name)
+			vars := t.VarVars(name.Name, field.Type, VarNew)
+			// v := t.NewVarExpr(name.Name, field.Type)
+			for _, v := range vars {
+				t.Printf("  input %v %v,\n", typeFmt(v.signed, v.size), v.name)
+			}
 		}
 	}
 	for i, field := range x.Type.Results.List {
 		if len(field.Names) != 0 {
 			panic(fmt.Errorf("unsupported return parameter names"))
 		}
-		v := t.NewVar(fmt.Sprintf("_out%v_", i), field.Type)
+		v := t.NewVarExpr(fmt.Sprintf("_out%v_", i), field.Type)
 		t.Printf("  output %v %v,\n", typeFmt(v.signed, v.size), v.name)
 		block.returnVars = append(block.returnVars, v)
 	}
@@ -436,7 +585,7 @@ func (t *Translator) Emit(node ast.Node) (err error) {
 			fmt.Println(string(trace))
 		}
 	}()
-	t.FindFuncs(node)
+	t.PassInit(node)
 	t.emit(node)
 	return nil
 }
@@ -446,14 +595,66 @@ type FuncArgs struct {
 	outputs []string
 }
 
-func (t *Translator) FindFuncs(node ast.Node) {
+type Struct struct {
+	name   string
+	fields []Var
+}
+
+func (t *Translator) ParseType(spec ast.Spec) {
+	switch s := spec.(type) {
+	case *ast.TypeSpec:
+		switch ts := s.Type.(type) {
+		case *ast.StructType:
+			st := Struct{name: s.Name.Name}
+			for _, f := range ts.Fields.List {
+				signed, size := t.DecodeType(f.Type)
+				for _, name := range f.Names {
+					st.fields = append(st.fields, Var{
+						name:   name.Name,
+						signed: signed,
+						size:   size,
+					})
+				}
+			}
+			t.structs[s.Name.Name] = st
+		default:
+			panic(fmt.Errorf("unsupported Spec.Type: %v", s.Type))
+		}
+	default:
+		panic(fmt.Errorf("unsupported Spec: %v", spec))
+	}
+}
+
+// PassInit performs some initial passes:
+// - Find all struct declarations
+// - Find all function declarations
+func (t *Translator) PassInit(node ast.Node) {
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.GenDecl:
+			if x.Tok == token.TYPE {
+				for _, spec := range x.Specs {
+					t.ParseType(spec)
+				}
+			}
+		}
+		return true
+	})
+
 	ast.Inspect(node, func(n ast.Node) bool {
 		switch x := n.(type) {
 		case *ast.FuncDecl:
 			var inputs []string
 			for _, field := range x.Type.Params.List {
 				for _, name := range field.Names {
-					inputs = append(inputs, fmt.Sprintf("%v0", name.Name))
+					if _, ok := t.typeInfo.TypeOf(field.Type).Underlying().(*types.Struct); ok {
+						typeName := t.typeInfo.TypeOf(field.Type).(*types.Named).Obj().Name()
+						for _, field := range t.structs[typeName].fields {
+							inputs = append(inputs, fmt.Sprintf("%v_%v0", name.Name, field.name))
+						}
+					} else {
+						inputs = append(inputs, fmt.Sprintf("%v0", name.Name))
+					}
 				}
 			}
 			var outputs []string
@@ -472,7 +673,7 @@ func (t *Translator) FindFuncs(node ast.Node) {
 func main() {
 	// parse file
 	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "samples/define.go", nil, parser.ParseComments|parser.SkipObjectResolution)
+	node, err := parser.ParseFile(fset, "samples/struct.go", nil, parser.ParseComments|parser.SkipObjectResolution)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ samples/struct.go
@@ -0,0 +1,16 @@
+package main
+
+type Data struct {
+	a uint8
+	b uint8
+}
+
+func entrypoint(a, b uint8) uint8 {
+	d := Data{a: a, b: b}
+	r := add(d)
+	return r
+}
+
+func add(d Data) uint8 {
+	return d.a + d.b
+}
